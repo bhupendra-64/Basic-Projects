91db60badc74dffa406f9ca2ad19417a229e979e
bhupendra pratap singh <bhupendras.ug24.ce@nitp.ac.in>
2024-12-29 16:46:24 +0530

    Add files via upload

---
+++ WebPractice/script.js
@@ -0,0 +1,13 @@
+// let btn=document.querySelector("button");
+// let body=document.querySelector("body");
+// let color="#3D2A2A";
+// btn.addEventListener("click",()=>{
+//     if(color=="#3D2A2A"){
+//         body.style.backgroundColor="#666b5e";
+//         color="#666b5e"
+//     }else{
+//         body.style.backgroundColor="#3D2A2A";
+//         color="#3D2A2A"
+
+//     }
+// })
