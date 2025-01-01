5e54043c3ff2bb4cc545f4f6734f38892399baa5
bhupendra pratap singh <bhupendras.ug24.ce@nitp.ac.in>
2025-01-01 11:26:09 +0530

    Add files via upload
    Instagram follow feature

---
+++ Friend request feature/script.js
@@ -0,0 +1,11 @@
+let btn=document.querySelector("button")
+
+
+btn.addEventListener("click",()=>{
+    btn.innerText="Following"
+    btn.style.backgroundColor="#0EAD69"
+    btn.style.color="White";
+    btn.disabled=true;
+   
+   
+})  
