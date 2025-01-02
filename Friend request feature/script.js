669859af4845cff69982d66b6fd384e456f24a06
bhupendra pratap singh <bhupendras.ug24.ce@nitp.ac.in>
2025-01-02 19:54:14 +0530

    Update script.js

--- Friend request feature/script.js
+++ Friend request feature/script.js
@@ -1,11 +1,22 @@
 let btn=document.querySelector("button")
-
-
+let a=1;
 btn.addEventListener("click",()=>{
-    btn.innerText="Following"
-    btn.style.backgroundColor="#0EAD69"
-    btn.style.color="White";
-    btn.disabled=true;
-   
-   
-})  
+    if(a==1){
+        btn.innerText="Following"
+        btn.style.backgroundColor="transparent"
+        btn.style.color="white";
+
+
+        a=0
+
+    }else{
+        btn.innerText="Follow"
+        btn.style.backgroundColor="#0095f6"
+        btn.style.color="White";
+        a=1
+    }
+    
+})    
+
+
+ 
